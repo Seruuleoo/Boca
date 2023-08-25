e44fcf36086325b006ee7b51ec7af6a32acb7c79
Seruuleoo <143140509+Seruuleoo@users.noreply.github.com>
2023-08-24 21:47:44 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-const musica = document.getElementById("musica");
-const imagen = document.getElementById("imagen");
+window.addEventListener('DOMContentLoaded', (event) => {
+    // Reproducir el audio automáticamente
+    const audio = document.getElementById('musica');
+    audio.play();
 
-// Reproducir música al cargar la página
-musica.play();
+    // Mostrar el mensaje "aguante boca"
+    const mensaje = document.createElement('div');
+    mensaje.innerText = 'aguante boca';
+    mensaje.style.color = 'darkblue';
+    mensaje.style.fontSize = '24px';
+    mensaje.style.textAlign = 'center';
+    mensaje.style.position = 'absolute';
+    mensaje.style.top = 'calc(20% - 2cm)'; // Ajusta la posición vertical
+    mensaje.style.left = '50%';
+    mensaje.style.transform = 'translateX(-50%)';
+    document.body.appendChild(mensaje);
+});
